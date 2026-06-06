6d5d8584eaace5adc938e363e6dbcd9a3b90959d
skmdsadiq1607 <skmdsadiq1607@gmail.com>
2026-06-06 23:07:57 +0530

    Update app name to ShopEZ

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
             <div className="container">
-                <Link className="navbar-brand fw-bold" to="/">StockEZ</Link>
+                <Link className="navbar-brand fw-bold" to="/">ShopEZ</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span className="navbar-toggler-icon"></span>
                 </button>
